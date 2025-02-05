eda8fb576a900c57a622bd024616fd0f089f6ada
Efe <efecagglar@gmail.com>
2025-02-05 14:43:09 -0500

    Sentry removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,8 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add these configurations
-  sentry: {
-    disableServerWebpackPlugin: true,
-    disableClientWebpackPlugin: true,
-  },
   // Disable static optimization for pages that use document
   experimental: {
     workerThreads: false,
     cpus: 1,
   },
 };
-
-// Modify Sentry config options
-const sentryWebpackPluginOptions = {
-  silent: true,
-  org: "javascript-mastery",
-  project: "javascript-nextjs",
-  // Disable source map upload during build
-  sourcemaps: {
-    disable: true,
-  },
-};
-
-const sentryOptions = {
-  widenClientFileUpload: true,
-  transpileClientSDK: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-};
-
-// Export with modified config
-export default withSentryConfig(
-  nextConfig,
-  sentryWebpackPluginOptions,
-  sentryOptions
-);
